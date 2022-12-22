22a5acaf6c528185cbe113b3007d74261d17bf6f
Potato10110 <mickoforescal@gmail.com>
2022-12-23 01:07:50 +0800

    service worker fixed

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,7 @@
 var CacheName = "App-cache";
-var CachesFiles = [
+var Assets = [
   "/",
   "/public",
-  "/public/metadata.json",
-  "/public/model.json",
-  "/public/weights.bin",
-  "/public/index.html",
-  "/public/homepage.html",
-  "/public/list.html",
-  "/public/profile.html",
-  "/public/recipeForm.html",
-  "/public/recipePage.html",
-  "/public/search.html",
-  "/public/Scan.html",
-  "/public/css/style.css",
-  "/public/css/login.css", 
-  "/public/css/list.css",
-  "/public/css/profile.css",
-  "/public/css/recipeForm.css",
-  "/public/css/recipePage.css",
-  "/public/css/search.css",
-  "/public/css/userRecipe.css",
-  "/public/css/scan.css",
-  "/public/js/main.js",
-  "/public/js/login.js",
-  "/public/js/list.js",
-  "/public/js/profile.js",
-  "/public/js/recipeForm.js",
-  "/public/js/recipePage.js",
-  "/public/js/search.js",
-  "/public/js/userRecipe.js",
-  "/public/js/homepage.js",
   "/public/images",
   "/public/images/food.jpg",
   "/public/icons",
@@ -38,8 +9,8 @@ var CachesFiles = [
 
 self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(CacheName).then(function (Cache) {
-      return Cache.addAll(CachesFiles);
+    caches.open(CacheName).then(function (Caches) {
+      return Caches.addAll(Assets);
     })
   );
   self.skipWaiting();
