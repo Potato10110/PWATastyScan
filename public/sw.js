c50ac1957ea327d5e9319136dd4b99a93e0d1914
Potato10110 <mickoforescal@gmail.com>
2022-12-26 02:30:55 +0800

    not fixed

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,34 @@
 var CacheName = "App-cache";
-var Assets = ["/", "/public", "/public/images", "/public/images/food.jpg"];
+var Assets = [
+  "/",
+  "/public",
+  "/public/index.html",
+  "/public/homepage.html",
+  "/public/list.html",
+  "/public/profile.html",
+  "/public/recipeForm.html",
+  "/public/recipePage.html",
+  "/public/search.html",
+  "/public/Scan.html",
+  "css/style.css",
+  "css/login.css",
+  "css/list.css",
+  "css/profile.css",
+  "css/recipeForm.css",
+  "css/recipePage.css",
+  "css/search.css",
+  "css/userRecipe.css",
+  "css/scan.css",
+  "js/main.js",
+  "js/login.js",
+  "js/list.js",
+  "js/profile.js",
+  "js/recipeForm.js",
+  "js/recipePage.js",
+  "js/search.js",
+  "js/userRecipe.js",
+  "js/homepage.js",
+];
 
 self.addEventListener("install", function (e) {
   e.waitUntil(
@@ -17,3 +46,4 @@ self.addEventListener("fetch", function (e) {
     })
   );
 });
+
