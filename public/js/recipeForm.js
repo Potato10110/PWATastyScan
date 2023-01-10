14564f00117fd939a175586e693d1531bbe781d8
Potato10110 <mickoforescal@gmail.com>
2023-01-11 04:12:46 +0800

    nothing

--- public/js/recipeForm.js
+++ public/js/recipeForm.js
@@ -38,26 +38,10 @@ const db = getFirestore(app);
 
 //db.settings({timestampsInSnapshots: true})
 
-// firebase (read data) in documentation
-/*const recipeSnapshot = await getDocs(collection(db, "UserRecipes"));
-recipeSnapshot.forEach((snapshot) => {
-  console.log(`${s.id} => ${doc.data()}`);
-});*/
 
 const recipeDB = collection(db, "UserRecipes");
 
-/*getDocs(recipeDB)
-  .then((snapshot) => {
-    let recipe = [];
-    snapshot.docs.forEach((doc) => {
-      recipe.push({ ...doc.data(), id: doc.id });
-    });
-    console.table(recipe);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-*/
+
 const addRecipe = document.querySelector("#recipeForm");
 /*addRecipe.addEventListener("submit", () => {
 
@@ -68,77 +52,7 @@ backBtn.addEventListener("click", () => {
   window.location.assign("./profile.html");
 });
 
-/*function validateForm() {
-  // Get the values of the input fields
-  var name = document.getElementById("userName").value;
-  var recipeName = document.getElementById("recipeName").value;
-  var time = document.getElementById("cookingTime").value;
-  var mealtype = document.getElementById("MealType").value;
-  var servings = document.getElementById("servings").value;
-  var ingredient = document.getElementById("ingredients").value;
-  var instruction = document.getElementById("instructions").value;
 
-  // Set the error messages to an empty string
-  document.getElementById("nameError").innerHTML = "";
-  document.getElementById("recipeError").innerHTML = "";
-  document.getElementById("timeError").innerHTML = "";
-  document.getElementById("mealError").innerHTML = "";
-  document.getElementById("servingError").innerHTML = "";
-  document.getElementById("ingredientError").innerHTML = "";
-  document.getElementById("instructionError").innerHTML = "";
-
-  // Flag variable to track whether there are any errors
-  var hasErrors = false;
-
-  // Validate the name field
-  if (name === "") {
-    document.getElementById("nameError").innerHTML = "Please enter your name";
-    hasErrors = true;
-  }
-
-  // Validate the email field
-  if (recipeName === "") {
-    document.getElementById("recipeError").innerHTML =
-      "Please enter a recipe name";
-    hasErrors = true;
-  }
-
-  if (time === "") {
-    document.getElementById("timeError").innerHTML =
-      "Please enter an estimate time of cooking";
-    hasErrors = true;
-  }
-
-  if (mealtype === "") {
-    document.getElementById("mealError").innerHTML = "Please pick a meal type";
-    hasErrors = true;
-  }
-
-  if (servings === "") {
-    document.getElementById("servingError").innerHTML =
-      "Please enter an  estimate servings";
-    hasErrors = true;
-  }
-
-  if (ingredient === "") {
-    document.getElementById("ingredientError").innerHTML =
-      "Please enter a list of ingredients";
-    hasErrors = true;
-  }
-
-  if (instruction === "") {
-    document.getElementById("instructionError").innerHTML =
-      "Please enter instructions";
-    hasErrors = true;
-  }
-
-  // Enable or disable the submit button based on whether there are any errors
-  document.getElementById("submitBtn").disabled = hasErrors;
-
-  // Return whether there were any errors
-  return !hasErrors;
-}
-*/
 
 const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", (e) => {
