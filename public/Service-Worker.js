4594803d48c302ae2de69a5cf83fbd9fb2becc9b
Potato10110 <mickoforescal@gmail.com>
2023-01-14 19:05:30 +0800

    small fix

--- public/Service-Worker.js
+++ public/Service-Worker.js
@@ -1,4 +1,4 @@
-var CacheName = "App-cache";
+var CacheName = "TastyScan-cache";
 var Assets = [
   "/",
   "/public",
@@ -14,11 +14,19 @@ var Assets = [
   "./js/login.js",
   "./icons/manifest-icon-192.maskable.png",
 ];
-
+/*This code will delete all the previous cache except the cache with the name
+"App-cache" when the service worker is installed. It will then add all the assets in 
+the "Assets" array to the new cache.  */
 self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(CacheName).then(function (Caches) {
-      return Caches.addAll(Assets);
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+          cacheNames.map(function(cacheName) {
+              if (cacheName !== CacheName) {
+                  return caches.delete(cacheName);
+              }
+          })
+      );
     })
   );
   self.skipWaiting();
